ba4b4212d8402003319a58174a450d0a50799d0c
theluismen <luismaocampo28@gmail.com>
2026-05-20 12:44:50 +0200

    feat: hanoi2.py tuneao

--- dobot/hanoi2.py
+++ dobot/hanoi2.py
@@ -5,28 +5,39 @@ from time import sleep
 from Garra_URV_api import Gripper
 from Utils_URV_Nova_api import Robot
 
+# Variables globales
+VEL_FAST=80
+VEL_SLOW=10
 
-def hanoi(mv, dsb, gr, n, origen, destino, auxiliar, torres, alturas):
+techo  = 250.00
+base   = 102.00
+offset = 10.00
+
+torres = {
+	"A": [-120,-364, 0.000000, 180.000000, 0.000000, 0.000000],
+	"B": [   0,-364, 0.000000, 180.000000, 0.000000, 0.000000],
+	"C": [ 120,-364, 0.000000, 180.000000, 0.000000, 0.000000]
+}
+
+
+def hanoi(mv, dsb, gr, n, origen, destino, auxiliar, alturas, vel_fast, vel_slow):
     if n == 1:
-        pick_and_place(mv, dsb, gr, torres, origen, destino, alturas)
+        pick_and_place(mv, dsb, gr, origen, destino, alturas, vel_fast, vel_slow)
         return
 
-    hanoi(mv, dsb, gr, n - 1, origen, auxiliar, destino, torres, alturas)
+    hanoi(mv, dsb, gr, n - 1, origen, auxiliar, destino, alturas, vel_fast, vel_slow)
 
-    pick_and_place(mv, dsb, gr, torres, origen, destino, alturas)
+    pick_and_place(mv, dsb, gr, origen, destino, alturas, vel_fast, vel_slow)
 
-    hanoi(mv, dsb, gr, n - 1, auxiliar, destino, origen, torres, alturas)
+    hanoi(mv, dsb, gr, n - 1, auxiliar, destino, origen, alturas, vel_fast, vel_slow)
 
 
-def pick_and_place(mv, dsb, gr, torres, origen, destino, alturas, vel_fast=80, vel_slow=10):
+def pick_and_place(mv, dsb, gr, origen, destino, alturas, vel_fast, vel_slow):
     """
     Realiza un ciclo de Pick and Place y finaliza en el punto de seguridad de destino.
     p_pick: Coordenadas reales de agarre [X, Y, Z, R, J, W]
     p_place: Coordenadas reales de entrega [X, Y, Z, R, J, W]
     """
-    techo  = 250.00
-    base   = 102.00
-    offset = 10.00
 
     p_pick     = torres[origen].copy()
     p_pick[2]  = base + len(alturas[origen]) * offset
@@ -79,10 +90,20 @@ def pick_and_place(mv, dsb, gr, torres, origen, destino, alturas, vel_fast=80, v
 
 if __name__ == '__main__':
 
+	# Argumentos de CLI
     parser = argparse.ArgumentParser(description="DoBot Hanoi")
-    parser.add_argument("-n", "--n", type=int, default=3, help="Numero de Discos")
+    parser.add_argument("-f", "--fast", action="store_true", help="Modo Rápido")
+    parser.add_argument("-n", "--n", type=int, required=True, help="Numero de Discos")
     args = parser.parse_args()
 
+    if args.n < 3 or args.n > 5:
+        exit("-n N: N tiene que estar en el intervalo [3..5]")
+        
+    if args.fast:
+        VEL_FAST=100
+        VEL_SLOW=100
+
+	# Inicializaciones
     dsb, mv, feed,feedFour, ip = Robot.ConnectRobot()
 
     feed_thread = threading.Thread(target=Robot.GetFeed, args=(feedFour,))
@@ -100,25 +121,35 @@ if __name__ == '__main__':
     # Habilitar Pinza
     print("[+] - Habilitando la pinza...")
     gr = Gripper(dsb, mv, ip)
-
-    torres = {
-        "A": [-120,-364, 0.000000, 180.000000, 0.000000, 0.000000],
-        "B": [   0,-364, 0.000000, 180.000000, 0.000000, 0.000000],
-        "C": [ 120,-364, 0.000000, 180.000000, 0.000000, 0.000000]
-    }
-
+	
+	# Inicializar las alturas
     alturas = {
-        "A": [i for i in range(args.n,0,-1)], # Pila Discos Inicial
-        "B": [],
-        "C": []
+	    "A": [i for i in range(args.n,0,-1)], # Pila Discos Inicial
+	    "B": [],
+	    "C": []
     }
 
+	# Colocarse En Posicion Normal
     mv.JointMovJ(0,0,0,0,0,0,60,50)
 
+	# Moverse a un punto inicial
+    print("[+] - Moviento Pinza a Inicio")
+    dsb.SpeedFactor(VEL_FAST)
     init_point = [0,-364, 205.000000, 180.000000, 0.000000, 0.000000]
-
+    
     mv.MovJ(init_point[0], init_point[1], init_point[2], init_point[3], init_point[4], init_point[5])
+    
+    init_point = torres["A"].copy()
+    init_point[2] = base + len(alturas["A"]) * offset
+    
+    mv.MovJ(init_point[0], init_point[1], init_point[2], init_point[3], init_point[4], init_point[5])
+    mv.Sync()
+    
+    input("[+] - Pulsa Enter para empezar...")
 
-    hanoi(mv, dsb, gr, args.n, "A", "C", "B", torres, alturas)
+	# Ejecutar hanoi con pick-and-place
+    hanoi(mv, dsb, gr, args.n, "A", "C", "B", alturas , VEL_FAST, VEL_SLOW)
 
+	# Volver a Posicion Normal
+    dsb.SpeedFactor(VEL_FAST)
     mv.JointMovJ(0,0,0,0,0,0,60,50)
